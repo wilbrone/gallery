be05b79067efab27da07ccbbdb7e215c149726f5
wilbrone <wilbroneokoth@gmail.com>
2020-02-02 22:07:32 +0300

    add image id to url

--- photos/urls.py
+++ photos/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns=[
     path('',views.all_photos,name='allPhotos'),
-    path('image/<id>/',views.single_image,name ='image'),
+    path('image/<image_id>/',views.single_image,name ='image'),
     path('search/', views.search_results, name='search_results'),
     path('location/<loct_id>/', views.location_pics, name='location'),
     # path()
